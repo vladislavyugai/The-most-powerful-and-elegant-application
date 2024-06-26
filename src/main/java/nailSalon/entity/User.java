a359b0188abfac9f2d378cf815ffb4112aa0b5f3
vladislavyugai <vladislav_yugai@mail.ru>
2024-06-26 21:20:22 +0400

    added missing annotation for User

--- src/main/java/nailSalon/entity/User.java
+++ src/main/java/nailSalon/entity/User.java
@@ -1,13 +1,18 @@
 package nailSalon.entity;
 
 import com.google.i18n.phonenumbers.Phonenumber.PhoneNumber;
+import org.springframework.stereotype.Component;
 
+@Component
 public class User {
     private static Integer id;
     private String name;
     private String surname;
 
     private PhoneNumber phoneNumber;
+
+    public User() {
+    }
 
     public User(String name, String surname, PhoneNumber phoneNumber) {
         id ++;
